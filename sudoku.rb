1d7a2d884c11406cb8aae5ebab067aa70a17b941
zachward <appacademy@Apps-Mac-mini-2.local>
2017-03-02 14:28:26 -0500

    Final check

--- sudoku.rb
+++ sudoku.rb
@@ -80,5 +80,7 @@ class SudokuGame
 end
 
 
-game = SudokuGame.from_file("puzzles/sudoku1.txt")
-game.run
+if __FILE__ == $PROGRAM_NAME
+  game = SudokuGame.from_file("puzzles/sudoku1-almost.txt")
+  game.run
+end
