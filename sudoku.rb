e036aa28942b7afd2187b9f63392fe798ca3b9ec
zachward <appacademy@Apps-Mac-mini-2.local>
2017-03-02 14:23:33 -0500

    Final check

--- sudoku.rb
+++ sudoku.rb
@@ -80,6 +80,7 @@ class SudokuGame
   attr_reader :board
 end
 
-
-game = SudokuGame.from_file("puzzles/sudoku1.txt")
-game.run
+if __FILE__ == $PROGRAM_NAME
+  game = SudokuGame.from_file("puzzles/sudoku1-almost.txt")
+  game.run
+end
